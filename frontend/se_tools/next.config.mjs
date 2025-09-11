021ac3d4d9111e71f16e2238b6053438bb2f149f
mbrowley24 <michael.a.browley@outlook.com>
2025-09-10 19:09:22 -0600

    before work push

--- frontend/se_tools/next.config.mjs
+++ frontend/se_tools/next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-    return [
-      { source: "/api/:path*", destination: "https://clownfish-app-3ncmz.ondigitalocean.app/:path*" },
-    ];
-  },
+//     async rewrites() {
+//     return [
+//       { source: "/api/:path*", destination: "https://clownfish-app-3ncmz.ondigitalocean.app/:path*" },
+//     ];
+//   },
 };
 
 export default nextConfig;
