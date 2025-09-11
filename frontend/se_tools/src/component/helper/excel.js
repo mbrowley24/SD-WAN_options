8c88f4cfab4a426ccc8356330bfd6d9376bbbcb9
mbrowley24 <michael.a.browley@outlook.com>
2025-09-10 22:24:50 -0600

    added file name to download disposition

--- frontend/se_tools/src/component/helper/excel.js
+++ frontend/se_tools/src/component/helper/excel.js
@@ -5,8 +5,9 @@ export const ExcelDownload = async (res) =>{
     const blob = await res.blob();
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
+    const name = filenameFromContentDisposition(cd);
     a.href = url;
-    a.download = `test.xlsx`;
+    a.download = `mx-Sales-config.xlsx`;
     a.click();
     URL.revokeObjectURL(url);
 }
